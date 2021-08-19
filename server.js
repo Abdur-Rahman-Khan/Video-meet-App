acebb884b73b3bbf4955f252f3c55baec997799c
Abdur-Rahman-Khan <71510046+Abdur-Rahman-Khan@users.noreply.github.com>
2021-08-19 12:54:59 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express=require('express');
 const app=express();
 const server=require('http').Server(app);
 const io=require('socket.io')(server);
+const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}))
 // const {v4:uuidV4}= require('uuid');
 
 
@@ -11,22 +13,38 @@ app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
     // res.redirect(`/${uuidV4()}`)
-    res.redirect(`/${randString()}`)
+    res.render('home');
+    // res.redirect(`/${randString()}`)
 
+});
+app.post('/',(req,res)=>{
+    // res.redirect(`/${uuidV4()}`)
+    res.render('home');
+    // res.redirect(`/${randString()}`)
+
+});
+app.post('/room',(req,res)=>{
+    // console.log(req,res);
+    // console.log((req.body.RoomID));
+    res.redirect(`/${req.body.RoomID}`)
+});
+
+app.post('/rand-room',(req,res)=>{
+    res.redirect(`/${randString()}`)
 });
 
 app.get('/:room',(req,res)=>{
     res.render('room',{roomId: req.params.room})
 });
 
-server.listen(3000,(e)=>{
+server.listen(process.env.PORT,(e)=>{
     console.log(`listening on port 3000`);
     
 });
 
 io.on('connection',(socket)=>{
     socket.on('join-room',(roomId,userId)=>{
-        console.log(roomId,userId);
+        // console.log(roomId,userId);
         socket.join(roomId);
         socket.to(roomId).emit('user-connected',userId);
         socket.on('disconnect',()=>{
